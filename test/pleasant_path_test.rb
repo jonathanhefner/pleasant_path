d89f975fced4bdbf3e5f7228b8b8cdc488e7703c
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-10-25 08:33:50 -0500

    Remove default test

--- test/pleasant_path_test.rb
+++ test/pleasant_path_test.rb
@@ -1,11 +1,9 @@
 require 'test_helper'
 
 class PleasantPathTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::PleasantPath::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
