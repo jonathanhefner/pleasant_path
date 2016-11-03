2c3961413266c31d25f19420b55d25da65c2dbc0
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-11-03 09:40:20 -0500

    Add File.edit_lines

--- lib/pleasant_path/file.rb
+++ lib/pleasant_path/file.rb
@@ -19,4 +19,24 @@ class File
     end
   end
 
+  # Reads from the specified file its contents as an array of lines, and
+  # yields the array to the given block for editing.  Writes the return
+  # value of the block back to the file, overwriting previous contents.
+  # The <code>$/</code> global string specifies what end-of-line
+  # characters to use for both reading and writing.  Returns the array
+  # of lines that comprises the file's new contents.
+  #
+  # @param filename [String, Pathname]
+  # @yield [lines] edits current file contents
+  # @yieldparam lines [Array<String>] current contents
+  # @yieldreturn [Array<String>] new contents
+  # @return [Array<String>]
+  def self.edit_lines(filename)
+    self.open(filename, 'r+') do |f|
+      lines = yield f.read_lines
+      f.seek(0, IO::SEEK_SET)
+      f.write_lines(lines)
+    end
+  end
+
 end
--- test/file_test.rb
+++ test/file_test.rb
@@ -13,4 +13,16 @@ class PleasantPath::FileTest < Minitest::Test
     end
   end
 
+  def test_edit_lines
+    Dir.mktmpdir do |tmp|
+      file = Pathname.new(tmp) / 'file'
+      text = "line 1\nline 2\n"
+      lines = text.split("\n")
+
+      file.write(text)
+      assert_equal lines.reverse, File.edit_lines(file, &:reverse)
+      assert_equal lines.reverse, file.read.split("\n")
+    end
+  end
+
 end
