62f9bb4f15d9fb9290d86b445e5f18f77839a8d4
Jonathan Hefner <jonathan.hefner@gmail.com>
2016-11-03 14:03:17 -0500

    Add assert_same_elements test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pleasant_path'
 
 require 'minitest/autorun'
+
+class Minitest::Test
+
+  def assert_same_elements(expected, actual)
+    assert_equal(expected.sort, actual.sort)
+  end
+
+end
